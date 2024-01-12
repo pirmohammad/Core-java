dc59564fc605f9e17cd342ffbacc34a02470e6e0
Pirmohammad <sheikh.pirmohammad@gmail.com>
2024-01-12 18:37:45 +0530

    done

---
+++ TestCollection/src/in/com/Collection/Test1.java
@@ -0,0 +1,14 @@
+package in.com.Collection;
+
+
+public class Test1 {
+	public static void main(String ar[]) {
+		String name="Pirmohammad sheikh indore";
+		System.out.println(name.length());
+		System.out.println(name.charAt(3));
+		System.out.println(name.indexOf("sheikh"));
+		System.out.println(name.indent('i'));
+		
+	}
+
+}
